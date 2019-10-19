862c0935a5273843197f087958cd1d487988b72f
chandandixit888 <56721890+chandandixit888@users.noreply.github.com>
2019-10-20 04:05:08 +0530

    Update Test.java

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,4 +1,10 @@
 
 public class Test {
+  public static void main(String[]args)
+  {
+    System.out.println("How to Add GitHub project to eclipse");
+    System.out.println("Commit and Push");
+    System.out.println("Any changes happens in programs(Commit and Push),Added Test class");
+    System.out.println("Pull the data from GitHub Repository");
 
 }
